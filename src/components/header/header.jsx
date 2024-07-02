e197a34ee018f22daa100ceb6030d0b94afcee4b
Caitlin <caitlinramsey@outlook.com>
2024-07-02 16:40:31 -0400

    Added socials

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,6 +1,8 @@
 import "./header.css";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import { FontAwesomeIcon } from '@fortawesome/free-brands-svg-icons';
+import { faFacebook, faInstagram } from '@fortawesome/free-brands-svg-icons';
 
 function handlePageChange(page) {
   console.log("Page changed to:", page);
@@ -20,6 +22,14 @@ export default function Header() {
             </Nav.Link>
             <Nav.Link href="/Contact" onClick={() => handlePageChange("Contact")}>
               Contact
+            </Nav.Link>
+          </Nav>
+          <Nav>
+            <Nav.Link href="https://www.facebook.com/digitalsirens" target="_blank">
+              <FontAwesomeIcon icon={faFacebook} />
+            </Nav.Link>
+            <Nav.Link href="https://www.instagram.com/digital.sirens" target="_blank">
+              <FontAwesomeIcon icon={faInstagram} />
             </Nav.Link>
           </Nav>
         </Navbar>
