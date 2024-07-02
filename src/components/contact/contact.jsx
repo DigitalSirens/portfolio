a4b7fd04c10741d938b995747588519192f71a2e
Stephanie <st3phanie.harris@gmail.com>
2024-07-02 16:32:46 -0400

    add: component templates

---
+++ src/components/contact/contact.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h1>Placeholder Contact Component</h1>
+      <p>
+        This is a placeholder component. Replace it with actual content during
+        development.
+      </p>
+    </div>
+  );
+};
+
+export default About;
