03b1fb3d78ba51e5a1995916ff6ec8fe447475ca
Stephanie <st3phanie.harris@gmail.com>
2024-07-02 16:02:54 -0400

    chore: updated react routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,37 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+
+import './index.css';
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Landing from './components/landing/landing';
+import Portfolio from './components/portfolio/portfolio';
+import Contact from './components/contact/contact';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '/',
+        element: <Landing />,
+      },
+      {
+        path: 'portfolio',
+        element: <Portfolio />,
+      },
+      {
+        path: 'contact',
+        element: <Contact />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
