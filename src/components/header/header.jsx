924d222619904d7a2932ff3d65ee7dca9baa3710
Caitlin <caitlinramsey@outlook.com>
2024-07-08 14:00:38 -0400

    Worked on header styling

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,8 +1,8 @@
 import "./header.css";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFacebook, faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFacebook, faInstagram } from "@fortawesome/free-brands-svg-icons";
 
 function handlePageChange(page) {
   console.log("Page changed to:", page);
@@ -13,24 +13,46 @@ export default function Header() {
     <>
       <div id="header">
         <Navbar bg="light">
-          <Nav className="me-auto text-black">
-            <Nav.Link href="/" onClick={() => handlePageChange("Home")}>
-              Home
-            </Nav.Link>
-            <Nav.Link href="/Portfolio" onClick={() => handlePageChange("About")}>
-              Portfolio
-            </Nav.Link>
-            <Nav.Link href="/Contact" onClick={() => handlePageChange("Contact")}>
-              Contact
-            </Nav.Link>
-          </Nav>
-          <Nav>
-            <Nav.Link href="https://www.facebook.com/digitalsirens" target="_blank">
-              <FontAwesomeIcon icon={faFacebook} />
-            </Nav.Link>
-            <Nav.Link href="https://www.instagram.com/digital.sirens" target="_blank">
-              <FontAwesomeIcon icon={faInstagram} />
-            </Nav.Link>
+          <Nav className="me-auto navbar-nav text-black">
+            <div className="nav-links">
+              <Nav.Link
+                className="home-link"
+                href="/"
+                onClick={() => handlePageChange("Home")}
+              >
+                Home
+              </Nav.Link>
+              <Nav.Link
+                className="portfolio-link"
+                href="/Portfolio"
+                onClick={() => handlePageChange("About")}
+              >
+                Portfolio
+              </Nav.Link>
+              <Nav.Link
+                className="contact-link"
+                href="/Contact"
+                onClick={() => handlePageChange("Contact")}
+              >
+                Contact
+              </Nav.Link>
+            </div>
+            <div className="social-links">
+              <Nav.Link
+                className="facebook-link"
+                href="https://www.facebook.com/digitalsirens"
+                target="_blank"
+              >
+                <FontAwesomeIcon icon={faFacebook} />
+              </Nav.Link>
+              <Nav.Link
+                className="instagram-link"
+                href="https://www.instagram.com/digital.sirens"
+                target="_blank"
+              >
+                <FontAwesomeIcon icon={faInstagram} />
+              </Nav.Link>
+            </div>
           </Nav>
         </Navbar>
       </div>
