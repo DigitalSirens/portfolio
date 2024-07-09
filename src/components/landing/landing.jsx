e46bd054e46297fda75e76991d899dd9594dfbb8
Caitlin <caitlinramsey@outlook.com>
2024-07-09 14:34:23 -0400

    Worked on landing page styling and added projects carousel

--- src/components/landing/landing.jsx
+++ src/components/landing/landing.jsx
@@ -1,21 +1,74 @@
-import "./landing.css"
+import "./landing.css";
+import React, { useRef, useState } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/effect-coverflow';
+import 'swiper/css/pagination';
+import { EffectCoverflow, Pagination } from 'swiper/modules';
 
 export default function Landing() {
-    return (
-        <>
-            <header id="heading">
-                <h1 className='welcome'>Welcome to Digital Sirens!</h1>
-            </header>
+  return (
+    <>
+      <div className="landing">
+        <header id="heading">
+          <h1 className="welcome">Welcome to Digital Sirens!</h1>
+        </header>
 
-            <div className='projects'>
-                <h1 className='projects-header'>Projects</h1>
-                <h2 className='carousel'>Carousel of projects goes here</h2>
-            </div>
+        <div className="projects">
+          <h1 className="projects-header">Projects</h1>
+          <h4 className='projects-desc'>Swipe to view</h4>
+        </div>
 
-            <div className='about'>
-                <h1 className='about-header'>About Us</h1>
-                <h4 className='about-desc'>About us description and photos</h4>
-            </div>
-        </>
-    )
+        <Swiper
+        effect={'coverflow'}
+        grabCursor={true}
+        centeredSlides={true}
+        slidesPerView={'auto'}
+        coverflowEffect={{
+          rotate: 50,
+          stretch: 0,
+          depth: 100,
+          modifier: 1,
+          slideShadows: true,
+        }}
+        pagination={true}
+        modules={[EffectCoverflow, Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-2.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-3.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-4.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-5.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-6.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-7.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-8.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-9.jpg" />
+        </SwiperSlide>
+      </Swiper>
+
+        <div className="about">
+          <h1 className="about-header">About Us</h1>
+          <h4 className="about-desc">About us description and photos</h4>
+        </div>
+      </div>
+    </>
+  );
 }
